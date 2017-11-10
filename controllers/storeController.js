8f6badf3b1f05126193805a9d7d2f96fdca09a6f
Ekeh Esom <mailesom@gmail.com>
2017-11-10 16:25:34 +0100

    tags store shuffle

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -87,7 +87,10 @@ exports.getStoreBySlug = async (req, res, next) => {
 
 // Get Store by tag
 exports.getStoreByTag = async (req,res) => {
-	const tags = await Store.getTagsList();
 	const tag = req.params.tag;
-	res.render('tag',{tags, title: 'Tags', tag })
+	const tagQuery = tag || { $exists: true, $ne: [] };
+	const tagsPromise = Store.getTagsList();
+	const storesPromise = Store.find({tags: tagQuery});
+	const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
+	res.render('tag',{tags, title: 'Tags', tag, stores });
 };
