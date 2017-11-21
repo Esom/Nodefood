13a09095dd39957af20d0818a6ef0ac470f1307a
Ekeh Esom <mailesom@gmail.com>
2017-11-21 18:28:12 +0100

    isLoggedIn middleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,18 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are now logged in!'
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You have been logged out');
+  res.redirect('/');
+};
+
+exports.isLoggedIn = (req, res, next) => {
+  if ( req.isAuthenticated()) {
+    next(); //carry on 
+    return;
+  }
+  req.flash('error', 'Oops you must be logged in');
+  res.redirect('/login');
+};
