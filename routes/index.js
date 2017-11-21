aca9c3e09b301f5e724aabf6b40582588cb53712
Ekeh Esom <mailesom@gmail.com>
2017-11-21 17:33:06 +0100

    passport local strategy

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
-const { catchErrors } = require('../handlers/errorHandlers');
+const authController = require('../controllers/authController');
+const {catchErrors} = require('../handlers/errorHandlers');
 
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
@@ -12,21 +13,22 @@ router.get('/stores/:id/edit', storeController.editStore);
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
 
 router.get('/add', storeController.addStore);
-router.post('/add', 
-  storeController.upload,
+router.post('/add', storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.createStore)
 );
 
-router.post('/add/:id', 
-  storeController.upload,
+router.post('/add/:id', storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.updateStore)
 );
 
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
-router.post('/register', userController.validateForm);
+router.post('/register', userController.validateRegister, 
+  catchErrors(userController.register),
+  authController.login
+);
 
 router.get('/tags', catchErrors(storeController.getStoreByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoreByTag));
