769f8279cce34e266e8d13eaf092d908cb218dfd
Ekeh Esom <mailesom@gmail.com>
2017-11-21 17:33:06 +0100

    passport local strategy

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
+const promisify = require('es6-promisify');
 
 exports.loginForm = (req, res) => {
 	res.render('login', {title: 'Login'});
@@ -9,12 +10,12 @@ exports.registerForm = (req, res) => {
 	res.render('register', {title: 'Register'});
 }
 
-exports.validateForm = (req, res, next) => {
+exports.validateRegister = (req, res, next) => {
 	req.sanitizeBody('name');
 	req.checkBody('name', 'You must supply a name!').notEmpty();
-	req.checkBody('name', 'That email is not valid!').isEmail();
+	req.checkBody('email', 'That email is not valid!').isEmail();
 	req.sanitizeBody('email').normalizeEmail({
-		remove_dots: false,
+		gmail_remove_dots: false,
 		remove_extension: false,
 		gmail_remove_subaddress: false
 	});
@@ -29,4 +30,11 @@ exports.validateForm = (req, res, next) => {
 		return; //stops the fn from running
 	}
 	next(); //there were no errors!
+};
+
+exports.register = async (req, res, next) => {
+	const user = new User({ email: req.body.email, name: req.body.name });
+	const register = promisify(User.register, User);
+	await register(user, req.body.password);
+	next();
 };
