b9a2ec19e0793c7614a553fe86b761f493282f25
Ekeh Esom <mailesom@gmail.com>
2017-11-23 02:19:24 +0100

    user permissions

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -50,9 +50,11 @@ exports.resize = async (req,res,next) => {
 }
 
 exports.createStore = async (req, res) => {
+	req.body.author = req.user._id;
+	console.log('author', req.body.author);
 	const store = await (new Store(req.body)).save();
 	req.flash('success', `Successfully created ${store.name}. Care to leave a review?`);
-	res.redirect('/store/${store.slug}');
+	res.redirect(`/store/${store.slug}`);
 };
 
 exports.getStores = async (req, res) => {
@@ -61,9 +63,16 @@ exports.getStores = async (req, res) => {
 	res.render('stores', {title: 'Stores' , stores });
 };
 
+const confirmOwner = (store, user) => {
+	if (!store.author.equals(user._id)) {
+		throw Error('You must own a store in order to edit it!');
+	}
+};
+
 exports.editStore = async (req, res) => {
-	// Find store by id
+	//Find store by id
 	const store = await Store.findOne({_id: req.params.id});
+	confirmOwner(store, req.user);
 	res.render('editStore', {title: `Edit ${store.name}`, store});
 };
 
@@ -80,7 +89,7 @@ exports.updateStore = async (req, res) => {
 
 // find store by slug
 exports.getStoreBySlug = async (req, res, next) => {
-	const store = await Store.findOne({slug: req.params.slug});
+	const store = await Store.findOne({slug: req.params.slug}).populate('author');
 	if(!store) return next();
 	res.render('store', {store, title: store.name});
 };
--- start.js
+++ start.js
@@ -29,3 +29,6 @@ app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
+// require mail handler
+// require('./handlers/mail');
